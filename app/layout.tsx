8454c084093f68a46090422f2f86d60a12fba188
William Li <email@williamli.dev>
2024-01-20 14:24:21 +0800

    added more test cases

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,6 +16,10 @@ export default function RootLayout({
   auth: React.ReactNode;
 }>) {
   const loginSegments = useSelectedLayoutSegment('auth');
+  const routeSegments = useSelectedLayoutSegment();
+  console.log({ loginSegments, routeSegments });
+  const loginSegmentsOutput = `${loginSegments} (${typeof loginSegments})`;
+  const routeSegmentsOutput = `${routeSegments} (${typeof routeSegments})`;
   return (
     <html lang="en">
       <body className={inter.className}>
@@ -23,9 +27,11 @@ export default function RootLayout({
           <nav className="flex space-x-2 text-blue-300 underline">
             <Link href="/">Main</Link>
             <Link href="/login">Login (/app/@auth/login)</Link>
+            <Link href="/reset">Reset (/app/@auth/reset)</Link>
             <Link href="/foo">Foo (regular page)</Link>
           </nav>
-          <div>loginSegments: {loginSegments}</div>
+          <div>loginSegments (parallel route): {loginSegmentsOutput}</div>
+          <div>routeSegmentsOutput (app route): {routeSegmentsOutput}</div>
           <section>{children}</section>
           <section>{auth}</section>
         </section>
